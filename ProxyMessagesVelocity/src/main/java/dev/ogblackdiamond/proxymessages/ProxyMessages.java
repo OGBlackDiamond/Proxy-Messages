39f72cd53b3e5282ef52018f9f9da82165707f8b
OGBlackDiamond <caden.feller@gmail.com>
2024-07-31 20:43:01 -0700

    added test output to the console

--- ProxyMessagesVelocity/src/main/java/dev/ogblackdiamond/proxymessages/ProxyMessages.java
+++ ProxyMessagesVelocity/src/main/java/dev/ogblackdiamond/proxymessages/ProxyMessages.java
@@ -1,8 +1,14 @@
 package dev.ogblackdiamond.proxymessages;
 
 import com.google.inject.Inject;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.ProxyServer;
+
+import java.nio.file.Path;
+
 import org.slf4j.Logger;
 
 /**
@@ -16,15 +22,25 @@ public class ProxyMessages {
 
     private final ProxyServer server;
     private final Logger logger;
+    private final Path dataDirectory;
 
     /**
      * Constructor, initializes the logger and the proxy server.
      */
     @Inject
-    public ProxyMessages(ProxyServer server, Logger logger) {
+    public ProxyMessages(ProxyServer server, Logger logger, @DataDirectory Path dataDirectory) {
         this.server = server;
         this.logger = logger;
+        this.dataDirectory = dataDirectory;
 
-        logger.info("Booting ProxyMessages.");
+        logger.info("Thank you for using ProxyMessages");
+        logger.warn("Testing warnings for ProxyMessages");
+        logger.error("Testing errors for ProxyMessages");
+    }
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+
+       // server.getEventManager().register(this, new PluginListener()); 
     }
 }
