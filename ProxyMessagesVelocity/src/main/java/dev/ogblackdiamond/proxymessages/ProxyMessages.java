a64298b73d5601d558e6966c87948184a76ee11a
Caden Feller <caden.feller@gmail.com>
2025-04-13 11:47:39 -0700

    updated version

--- ProxyMessagesVelocity/src/main/java/dev/ogblackdiamond/proxymessages/ProxyMessages.java
+++ ProxyMessagesVelocity/src/main/java/dev/ogblackdiamond/proxymessages/ProxyMessages.java
@@ -38,7 +38,7 @@ import org.spongepowered.configurate.yaml.YamlConfigurationLoader;
 /**
  * Main class for ProxyMessages.
  */
-@Plugin(id = "proxymessages", name = "ProxyMessages", version = "3.0.0",
+@Plugin(id = "proxymessages", name = "ProxyMessages", version = "3.0.1",
     description = "A message system for servers to interact over a proxy.", 
     authors = {"BlackDiamond"})
 public class ProxyMessages {
